9db902e2e98ceb7d3e5104a3753ba8ead03e6dc5
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2023-05-11 18:16:05 -0300

    ChessGame

---
+++ src/main/java/org/example/chess/ChessPosition.java
@@ -0,0 +1,37 @@
+package org.example.chess;
+
+import org.example.boardgame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if ( column < 'a' || column > 'h' || row < 1 || row > 8)throw new ChessException("Posições inexistentes no jogo." +
+                " Valores válidos a partir do a até o h e do 1 até o 8");
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8-row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8- position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return ""+column + row;
+    }
+}
